27b5a3577a8a27bf3037dd7a75d0a6ee766e0896
GuusvanMeel <vanmeelguus@gmail.com>
2026-09-06 00:13:40 +0200

    Update page.tsx

--- app/LoveableAI/page.tsx
+++ app/LoveableAI/page.tsx
@@ -1,16 +1,20 @@
 "use client";
 
-import { Layout } from './components/Layout';
-import { CurrentBooks } from './components/CurrentBooks';
-import { SpinWheel } from './components/SpinWheel';
-import { ReadingCalendar } from './components/ReadingCalendar';
+import { Suspense } from "react";
+import { Layout } from "./components/Layout";
+import { CurrentBooks } from "./components/CurrentBooks";
+import { SpinWheel } from "./components/SpinWheel";
+import { ReadingCalendar } from "./components/ReadingCalendar";
 
 export default function HomePage() {
   return (
     <Layout title="Leeslijst">
       <CurrentBooks />
       <SpinWheel />
-      <ReadingCalendar />
+
+      <Suspense fallback={null}>
+        <ReadingCalendar />
+      </Suspense>
     </Layout>
   );
 }
